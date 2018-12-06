30acf9b17373ba6a5c9ce8dcda98728759f794af
programmiri <programmiri@gmail.com>
2018-12-06 21:52:52 +0100

    Solve Day 1, task 2

--- tasks/day01.js
+++ tasks/day01.js
@@ -1,4 +1,24 @@
 function calcFrequency(input) {
   return input.reduce((accumulator, currVal) => accumulator + currVal);
 }
-export default calcFrequency;
+
+
+function returnFirstRepeatingFrequency(input) {
+
+  const reachedFrequencies = new Set([0]);
+  let frequency  = 0;
+
+  while(true) {
+    for (let i = 0; i < input.length; i++) {
+      frequency += input[i];
+      if (reachedFrequencies.has(frequency)) {
+          return frequency;
+      }
+      reachedFrequencies.add(frequency);
+    }
+  }
+}
+
+export { calcFrequency, returnFirstRepeatingFrequency }
+
+
--- tasks/day01.test.js
+++ tasks/day01.test.js
@@ -1,24 +1,49 @@
-import calcFrequency from './day01'
+import {calcFrequency, returnFirstRepeatingFrequency} from './day01'
+
+describe('calcFrequency', ()=>{
+  it('gives back the frequency 3', () => {
+    const inputExample = [+1, -2, +3, +1]
+    expect(calcFrequency(inputExample)).toBe(3);
+  });
+
+  it('gives back the frequency 3', () => {
+    const inputExample = [+1, +1, +1]
+    expect(calcFrequency(inputExample)).toBe(3);
+  });
 
 
-it('gives back the frequency', () => {
-  const inputExample = [+1, -2, +3, +1]
-  expect(calcFrequency(inputExample)).toBe(3);
-});
-
-it('gives back the frequency', () => {
-  const inputExample = [+1, +1, +1]
-  expect(calcFrequency(inputExample)).toBe(3);
-});
+  it('gives back the frequency 0', () => {
+    const inputExample = [+1, +1, -2]
+    expect(calcFrequency(inputExample)).toBe(0);
+  });
 
 
-it('gives back the frequency', () => {
-  const inputExample = [+1, +1, -2]
-  expect(calcFrequency(inputExample)).toBe(0);
-});
+  it('gives back the frequency -6', () => {
+    const inputExample = [-1, -2, -3]
+    expect(calcFrequency(inputExample)).toBe(-6);
+  });
+})
+
+describe('returnFirstRepeatingFrequency', ()=>{
+  it('gives "0" back as the first frequency twice reached', () => {
+    const inputExample = [+1, -1]
+    expect(returnFirstRepeatingFrequency(inputExample)).toBe(0);
+  });
+
+  it('gives "10" back as the first frequency twice reached', () => {
+    const inputExample = [+3, +3, +4, -2, -4]
+    expect(returnFirstRepeatingFrequency(inputExample)).toBe(10);
+  });
 
 
-it('gives back the frequency', () => {
-  const inputExample = [-1, -2, -3]
-  expect(calcFrequency(inputExample)).toBe(-6);
-});
+  it('gives "5" back as the first frequency twice reached', () => {
+    const inputExample = [-6, +3, +8, +5, -6]
+    expect(returnFirstRepeatingFrequency(inputExample)).toBe(5);
+  });
+
+
+  it('gives "14" back as the first frequency twice reached', () => {
+    const inputExample = [+7, +7, -2, -7, -4]
+    expect(returnFirstRepeatingFrequency(inputExample)).toBe(14);
+  });
+})
