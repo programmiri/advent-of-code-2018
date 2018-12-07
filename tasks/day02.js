0e0ac3df5335950f2b46cd3446cb8570fcb7c9a7
programmiri <programmiri@gmail.com>
2018-12-07 23:30:09 +0100

    Refactor day 2 task 1 to make it more functional :metal:

--- tasks/day02.js
+++ tasks/day02.js
@@ -1,24 +1,40 @@
+function countChars(ids) {
+  return ids.split('').reduce((acc, char) => {
+    acc[char] =  acc[char] ? acc[char] + 1 : 1;
+    return acc;
+  }, {})
+}
+
+function findMultiples(charsPerId) {
+  const filterBy = (filter, key) => charsPerId[key] == filter
+
+  return {
+    foundTwo: Object.keys(charsPerId).find(filterBy.bind(null, 2)),
+    foundThree: Object.keys(charsPerId).find(filterBy.bind(null, 3))
+  }
+}
+
+function sumUpMultiple(acc, curr) {
+    if(curr.foundTwo) {
+      acc.twoMatches += 1
+    }
+
+    if(curr.foundThree) {
+      acc.threeMatches += 1
+    }
+
+    return acc;
+}
+
+
+
 function getCheckSum(input) {
-  let twoMatches = 0;
-  let threeMatches = 0;
+  const r = input
+    .map(countChars)
+    .map(findMultiples)
+    .reduce(sumUpMultiple, { twoMatches: 0, threeMatches: 0 });
 
-  input.forEach((str) => {
-    const found = str.split('').reduce((acc, char) => {
-      acc[char] =  acc[char] ? acc[char] + 1 : 1;
-      return acc;
-    }, {})
-    const foundTwo = Object.keys(found).find( (key) => found[key] == 2)
-    const foundThree = Object.keys(found).find( (key) => found[key] == 3)
-
-    if(foundTwo) {
-      twoMatches++
-    }
-
-    if(foundThree) {
-      threeMatches++
-    }
-  })
-  return twoMatches * threeMatches;
+  return r.twoMatches * r.threeMatches;
 }
 
 export {getCheckSum}
